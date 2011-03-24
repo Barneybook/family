c6e9f4ce53cfef5f08a94b249e6b1863f340d754
barneybook <barneybook@gmail.com>
2011-03-24 12:34:46 +0800

    add init files

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿
+<?php												
+
+include "header.php";
+
+echo "<a href='register.php'>註冊資料</a>";
+echo "<p>Hello, World!!</p>";
+
+echo "test git"
+
+
+?>
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh_TW">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title>註冊頁面</title>
+</head>
+<body>
+
+	
+	<form action="register.php">
+	
+		<p>姓名：<input type="text" name="Name" value="姓名"/></p>
+		<p>性別：<input type="text" name="Sex"	value="性別"/></p>
+		<p>信箱：<input type="text" name="email"value="信箱"/></p>
+		<p>性別：<input type="text" name=""		value=""/></p>
+		<p><input type="text" name=""		value=""/></p>
+		<p><input type="text" name=""		value=""/></p>
+		<p><input type="text" name=""		value=""/></p>
+		
+		<input type="submit" />
+	</form>
+	
+
+
+</body>
+</html>

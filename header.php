8a7b7602bb28a2646417440fefeb66ba9b85d9fe
barneybook <barneybook@gmail.com>
2011-03-26 00:33:29 +0800

    edit some value

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh_TW">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<link rel="stylesheet" href="css/style.css" />
 	<title><?php echo $title_Name; ?></title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,13 +3,29 @@
 
 include "header.php";
 
-echo "<a href='register.php'>註冊資料</a>";
-echo "<p>Hello, World!!</p>";
-
-echo "test git"
-
-
 ?>
+<!--
+<div class="bar"><a href="register.php">註冊資料</a></div>
+-->
+<div class="content">
+
+<div class="top">
+
+</div>
+<div class="menu">
+
+<div class="bar"><a href="new.php">增加人員</a></div>
+<div class="bar"><a href="edit.php">修改資料</a></div>
+<div class="bar"><a href="show.php">列出族譜</a></div>
+<div class="bar"><a href="list.php">列出清單</a></div>
+
+</div>
+
+<div class="data">
+
+</div>
+
+</div>
 
 
 </body>
